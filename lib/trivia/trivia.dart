8ab753fc711260368b42339a33c5393b5bdbc7e1
Heli Perez <helip@powerstarmex.com>
2023-12-03 16:01:17 -0600

    Juego de trivia

--- lib/trivia/trivia.dart
+++ lib/trivia/trivia.dart
@@ -1,7 +1,11 @@
 import 'dart:js_interop';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:minijuegos_jw/trivia/trivia_logic.dart';
+
+import '../widgets/countdown_timer.dart';
+import '../widgets/fitted_text.dart';
 
 class Trivia extends StatefulWidget {
   const Trivia({Key? key}) : super(key: key);
@@ -12,14 +16,18 @@ class Trivia extends StatefulWidget {
 
 class _TriviaState extends State<Trivia> {
   final TriviaLogic _logic = TriviaLogic();
+  int _streak = 0;
+  int _level = 1;
+  final GlobalKey<QuestionWidgetState> _questionWidgetKey =
+      GlobalKey<QuestionWidgetState>();
 
   @override
   void initState() {
     super.initState();
-    next();
+    _init();
   }
 
-  void next() {
+  void _init() {
     _logic.ready.then((_) {
       setState(() {
         _logic.init();
@@ -27,10 +35,54 @@ class _TriviaState extends State<Trivia> {
     });
   }
 
-  void evaluate(text) {
-    if (text == _logic.currentPregunta!.correcta) {
-      next();
+  void _next() {
+    setState(() {
+      _logic.nextQuestion(_level);
+      _questionWidgetKey.currentState?.restart();
+    });
+  }
+
+  Future<void> _onSelect(String text) async {
+    String title = '';
+    String content = '';
+    if (text == _logic.currentQuestion!.correct) {
+      title = 'Respuesta correcta 😁👍';
+      _streak++;
+      _level = _level == 6 ? 1 : _level + 1;
+      content = 'Racha de preguntas correctas: $_streak';
     }
+    else {
+      title = 'Respuesta incorrecta 😓';
+      content = 'Racha conseguida: $_streak';
+      if (text == 'timeout') {
+        title = 'Se te acabo el tiempo ⏲️';
+      }
+      _streak = 0;
+      _level = 1;
+    }
+    await _showDialog(title, content, context);
+    _next();
+  }
+
+  Future<String?> _showDialog(
+      String title, String content, BuildContext context) async {
+    return await showDialog<String>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) => AlertDialog(
+        title: Text(title),
+        content: Text(content),
+        actions: <Widget>[
+          ElevatedButton.icon(
+            onPressed: () async {
+              Navigator.pop(context, 'Siguiente');
+            },
+            icon: const Icon(Icons.skip_next),
+            label: const Text('Siguiente'),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -39,58 +91,155 @@ class _TriviaState extends State<Trivia> {
       appBar: AppBar(
         title: const Text('Trivia Bíblico'),
       ),
-      body: Column(
-        children: [
-          if (!_logic.currentPregunta.isNull) ...{
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: Text(
-                _logic.currentPregunta!.pregunta,
-                style:
-                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+      body: Stack(
+        children: <Widget>[
+          // Imagen de fondo
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/trivia_bg.png"), // Reemplaza con tu imagen
+                fit: BoxFit.cover,
               ),
             ),
-            Expanded(
-              child: ListView.builder(
-                padding: const EdgeInsets.all(12),
-                itemBuilder: (context, index) {
-                  final text = _logic.currentPregunta!.opciones[index];
-                  return Option(
-                    index: index + 1,
-                    text: text,
-                    onTap: () {
-                      evaluate(text);
-                    },
-                  );
-                },
-                itemCount: _logic.currentPregunta!.opciones.length,
-              ),
+          ),
+          // Efecto Blur
+          BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 2.0, sigmaY: 2.0), // Ajusta el nivel de blur
+            child: Container(
+              color: Colors.black.withOpacity(0.0), // Color transparente
             ),
-          },
+          ),
+          // Contenido del Scaffold
+          (!_logic.currentQuestion.isNull)
+          ? QuestionWidget(
+              key: _questionWidgetKey,
+              question: _logic.currentQuestion!,
+              onSelect: _onSelect,
+            )
+          : const Text('Loading...'),
         ],
+      ),      
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        color: Theme.of(context).colorScheme.primary,
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(5, 5, 70, 5),
+          child: Wrap(
+            alignment: WrapAlignment.center,
+            spacing: 5,
+            runSpacing: 5,
+            children: [
+              Text("Dificultad: $_level/6"),
+              Text("Racha: $_streak"),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
 
-class Option extends StatelessWidget {
+class QuestionWidget extends StatefulWidget {
+  final Question question;
+  final void Function(String) onSelect;
+
+  const QuestionWidget(
+      {Key? key, required this.question, required this.onSelect})
+      : super(key: key);
+
+  @override
+  State<QuestionWidget> createState() => QuestionWidgetState();
+}
+
+class QuestionWidgetState extends State<QuestionWidget> {
+  final GlobalKey<CountdownTimerWidgetState> _timerKey =
+      GlobalKey<CountdownTimerWidgetState>();
+
+  void _select(text) {
+    widget.onSelect(text);
+    _timerKey.currentState?.stop();
+  }
+
+  void restart() {
+    _timerKey.currentState?.restart();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CountdownTimerWidget(
+          key: _timerKey,
+          initialTimeInSeconds: 60 * 1,
+          onTimeEnd: () => _select('timeout'),
+        ),
+        Card(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Text(
+              widget.question.text,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ),        
+        Expanded(
+          child: ListView.builder(
+            padding: const EdgeInsets.all(12),
+            itemBuilder: (context, index) {
+              final text = widget.question.options[index];
+              return OptionWidget(
+                index: index + 1,
+                text: text,
+                onTap: () {
+                  _select(text);
+                },
+              );
+            },
+            itemCount: widget.question.options.length,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class OptionWidget extends StatelessWidget {
   final int index;
   final String text;
   final VoidCallback? onTap;
 
-  const Option({Key? key, required this.index, required this.text, this.onTap})
+  const OptionWidget(
+      {Key? key, required this.index, required this.text, this.onTap})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: CircleAvatar(
-        backgroundColor: Theme.of(context).colorScheme.secondary,
-        child: Text('$index'),
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
       ),
-      title: Text(text),
-      onTap: onTap,
-      contentPadding: const EdgeInsets.all(10)
+      child: ListTile(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        leading: CircleAvatar(
+          backgroundColor: Theme.of(context).colorScheme.secondary,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FittedText(
+              '$index',
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).colorScheme.background,
+            ),
+          ),
+        ),
+        title: Text(text),
+        onTap: onTap,
+        contentPadding: const EdgeInsets.all(10),
+      ),
     );
   }
 }
--- lib/trivia/trivia_logic.dart
+++ lib/trivia/trivia_logic.dart
@@ -1,13 +1,14 @@
 import 'dart:async' show Future;
 import 'dart:math';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
 class TriviaLogic {
   late Future ready;
-  final List<Pregunta> _preguntas = [];
+  final List<Question> _questions = [];
   final Random _random = Random();
   Random get random => _random;
-  Pregunta? currentPregunta;
+  Question? currentQuestion;
 
   TriviaLogic() {
     ready = _loadAsset('assets/files/trivia.txt').then((text) {
@@ -16,25 +17,28 @@ class TriviaLogic {
         var max = 0;
         var min = 100000;
         var list = text.split('\n');
-        for (var line in list) {          
-          //print(line);
+        for (var line in list) {
           var split = line.split('\t');
-          var pregunta = split[0];
-          var correcta = split[1];
-          var erroneas = split[2].split(',').map((e) => e.trim()[0].toUpperCase() + e.trim().substring(1)).toList();
-          var nivel = int.parse(split[3]);
-          _preguntas.add(Pregunta(pregunta, correcta, erroneas, nivel));
+          var text = split[0];
+          var correct = split[1];
+          var incorrects = split[2].split(';').map((e) => e.trim()[0].toUpperCase() + e.trim().substring(1)).toList();
+          var level = int.parse(split[3]);
+          _questions.add(Question(text, correct, incorrects, level));
           count ++;
-          max = max > erroneas.length ? max : erroneas.length;
-          min = min < erroneas.length ? min : erroneas.length;
+          max = max > incorrects.length ? max : incorrects.length;
+          min = min < incorrects.length ? min : incorrects.length;
         }
-        print('Count: ' + count.toString());
-        print('Max: ' + max.toString());
-        print('Min: ' + min.toString());
+        if (kDebugMode) {
+          print('Count: $count');
+          print('Max: $max');
+          print('Min: $min');
+        }
 
       } catch(e, stacktrace) {
-        print('Exception: ' + e.toString());
-        print('Stacktrace: ' + stacktrace.toString());
+        if (kDebugMode) {
+          print('Exception: $e');
+          print('Stacktrace: $stacktrace');
+        }
       }
     });
   }
@@ -43,33 +47,34 @@ class TriviaLogic {
     return await rootBundle.loadString(key);
   }
 
-  Pregunta nextPregunta() {
-    currentPregunta = _preguntas[_random.nextInt(_preguntas.length)];
-    currentPregunta!.prepare(_random);
-    return currentPregunta!;
+  Question nextQuestion(int level) {
+    var questionsLevel = _questions.where((element) => element.level == level).toList();
+    currentQuestion = questionsLevel[_random.nextInt(questionsLevel.length)];
+    currentQuestion!.prepare(_random);
+    return currentQuestion!;
   }
 
   bool init() {
-    nextPregunta();
+    nextQuestion(1);
     return true;
   }
 }
 
-class Pregunta {
-  String pregunta;
-  String correcta;
-  List<String> erroneas;
-  int nivel;
-  List<String> opciones = [];
+class Question {
+  String text;
+  String correct;
+  List<String> incorrects;
+  int level;
+  List<String> options = [];
   
-  Pregunta(this.pregunta, this.correcta, this.erroneas, this.nivel);
+  Question(this.text, this.correct, this.incorrects, this.level);
   
   void prepare(Random random) {
-    opciones.clear();
-    opciones.add(correcta);
-    erroneas.shuffle(random);
-    opciones.addAll(erroneas.take(3));
-    opciones.shuffle(random);
+    options.clear();
+    options.add(correct);
+    incorrects.shuffle(random);
+    options.addAll(incorrects.take(3));
+    options.shuffle(random);
   }
 }
 
